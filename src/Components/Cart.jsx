88301be732120dd8184693c2d17273630845f602
mercy chepkemoi koech <koechmercyk@gmail.com>
2025-04-15 22:22:54 +0300

    stage 1

---
+++ src/Components/Cart.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Cart() {
+    return (
+      <div className="cart">
+        <h2>Shopping Cart</h2>
+        <p>Your cart is empty.</p>
+      </div>
+    );
+}
+export default Cart;
